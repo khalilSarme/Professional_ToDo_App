32394dab14a9711d380c56db64aa5c8ffc7204e1
KhalilSarme <ks8929801@gmail.com>
2025-07-28 11:07:21 +0300

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,7 +14,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  base: '/Professional_ToDo_App/',
   plugins: [
     VueRouter(),
     Layouts(),
